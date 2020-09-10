10eb82dd49270ccdc75a49bbfbe44dc5a4ab91af
elite_mlgbro <you@example.com>
2020-09-10 07:34:35 -0600

    test = unsuccessful

--- src/main/java/nukesfromthefuture/command/Test.java
+++ src/main/java/nukesfromthefuture/command/Test.java
@@ -1,7 +1,10 @@
 package nukesfromthefuture.command;
 
+import net.minecraft.client.particle.EntityHugeExplodeFX;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
@@ -23,12 +26,12 @@ public class Test extends CommandBase{
 
     @Override
     public String getCommandUsage(ICommandSender p_71518_1_) {
-        return "/" + getCommandName();
+        return "/" + getCommandName() + x + y + z;
     }
 
     @Override
     public void processCommand(ICommandSender uh, String[] p_71515_2_) {
         uh.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_RED + "Succ-essfully detonated"));
-        worldObj.spawnEntityInWorld(new EntityColliderBlast(worldObj, x, y, z, stuff, Nukesfromthefuture.colliderStrength));
+
     }
 }

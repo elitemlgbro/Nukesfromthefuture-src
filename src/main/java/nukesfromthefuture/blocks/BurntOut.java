eccfad56660fea420545bdba0a391e318a062060
elite_mlgbro <you@example.com>
2020-09-10 07:30:48 -0600

    even reactors burn out

---
+++ src/main/java/nukesfromthefuture/blocks/BurntOut.java
@@ -0,0 +1,12 @@
+package nukesfromthefuture.blocks;
+
+
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BurntOut extends Block {
+    public BurntOut(Material oo){
+        super(oo);
+    }
+}

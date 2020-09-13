bde13eeb3e1f4ad293b916c109c80961ef883fd3
elite_mlgbro <you@example.com>
2020-09-12 19:14:48 -0600

    Finished Nether Spire :D

--- src/main/java/nukesfromthefuture/tileentity/TileNReactor.java
+++ src/main/java/nukesfromthefuture/tileentity/TileNReactor.java
@@ -2,23 +2,17 @@ package nukesfromthefuture.tileentity;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityPigZombie;
-import net.minecraft.entity.projectile.EntityEgg;
+
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import nukesfromthefuture.Nukesfromthefuture;
-import nukesfromthefuture.blocks.NetherReact;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class TileNReactor extends TileEntity{
     public int age = 0;
@@ -45,13 +39,34 @@ public class TileNReactor extends TileEntity{
     }
 
     public void buildSpire(World world, int x, int y, int z) {
-        for(int j = 0; j < 12; j++){
-            for (int i = 0; i < 12; i++){
-                world.setBlock((x - 5) + i, y - 2, (z - 5) + j, Blocks.netherrack);
-                world.setBlock(x + 6, (y - 2) + i, (z - 5) + j, Blocks.netherrack);
-                world.setBlock(x - 6, (y - 2) + i, (z - 5) + j, Blocks.netherrack);
-                world.setBlock((x - 5) + j, (y - 2) + i, z + 6, Blocks.netherrack);
-                world.setBlock((x - 5) + j, (y - 2) + i, z - 6, Blocks.netherrack);
+        //YAS, I did this all in my head *laughs in no brain cells*
+        if(!world.isRemote) {
+            for (int j = 0; j < 12; j++) {
+                for (int i = 0; i < 12; i++) {
+                    world.setBlock((x - 5) + i, y - 2, (z - 5) + j, Blocks.netherrack);
+                    world.setBlock(x + 6, (y - 2) + i, (z - 5) + j, Blocks.netherrack);
+                    world.setBlock(x - 6, (y - 2) + i, (z - 5) + j, Blocks.netherrack);
+                    world.setBlock((x - 5) + j, (y - 2) + i, z + 6, Blocks.netherrack);
+                    world.setBlock((x - 5) + j, y + 26, z + 6, Blocks.nether_brick_fence);
+                    world.setBlock((x + 5) + j, y + 26, z + 6, Blocks.nether_brick_fence);
+                    world.setBlock((x - 5) + j, (y - 2) + i, z - 6, Blocks.netherrack);
+                    for(int l = 0; l < 13; l++) {
+                        for (int k = 0; k < 13; k++) {
+                            world.setBlock((x - 6) + k, y + 10, (z - 6) + l, Blocks.netherrack);
+                            world.setBlock((x - 6) + k,y + 25, (z - 6) + l, Blocks.nether_brick);
+                        }
+                    }
+                    for(int p = 0; p < 14; p++) {
+                        for (int n = 0; n < 14; n++) {
+                            for (int o = 0; o < 7; o++) {
+                                world.setBlock((x - 3) + o, (y + 11) + p, z + 3, Blocks.nether_brick);
+                                world.setBlock(x + 3, (y + 11) + p, (z - 3) + o, Blocks.nether_brick);
+                                world.setBlock((x - 3) + o, (y + 11) + p, z - 3, Blocks.nether_brick);
+                                world.setBlock(x - 3, (y + 11) + p, (z - 3) + o, Blocks.nether_brick);
+                            }
+                        }
+                    }
+                }
             }
         }
 
@@ -98,7 +113,6 @@ public class TileNReactor extends TileEntity{
     @Override
     public void updateEntity() {
 
-    EntityPigZombie entity = new EntityPigZombie(worldObj);
         if(this.Activated()) {
             age ++;
             if (age == 1000) {
@@ -133,24 +147,27 @@ public class TileNReactor extends TileEntity{
                 worldObj.setBlock(xCoord, yCoord, zCoord, Nukesfromthefuture.reactor_burnt_out);
             }
             if(!worldObj.isRemote) {
-                if (age == 5) {
+                if (age == 200) {
+                    EntityPigZombie entity = new EntityPigZombie(worldObj);
                     entity.setPosition(xCoord + 4, yCoord - 1, zCoord);
                     worldObj.spawnEntityInWorld(entity);
+
                 }
-                if (age == 7) {
+                if (age == 214) {
+                    EntityPigZombie entity = new EntityPigZombie(worldObj);
                     entity.setPosition(xCoord + 4, yCoord - 1, zCoord - 3);
                     worldObj.spawnEntityInWorld(entity);
                 }
-                if (age == 10 || age == 20 || age == 31) {
+                if (age == 50 || age == 100 || age == 150) {
+                    EntityPigZombie entity = new EntityPigZombie(worldObj);
                     entity.setPosition(xCoord - 4, yCoord - 1, zCoord + 3);
                     worldObj.spawnEntityInWorld(entity);
-
-
-
+                }
+                if (age == 20) {
                     for (int i = 0; i < 11; i++) {
                         for (int j = 0; j < 11; j++) {
-                            EntityItem sthuff = new EntityItem(worldObj, (xCoord - 5)+ i, yCoord - 1, (zCoord - 5) + j, new ItemStack(Items.blaze_rod));
-                            EntityItem more = new EntityItem(worldObj, (xCoord - 5) + i, yCoord - 1, (zCoord - 5) + j, new ItemStack(Items.record_11));
+                            EntityItem sthuff = new EntityItem(worldObj, (xCoord - 5) + i, yCoord - 1, (zCoord - 5) + j, new ItemStack(Items.blaze_rod));
+                            EntityItem more = new EntityItem(worldObj, (xCoord - 5) + i, yCoord - 1, (zCoord - 5) + j, new ItemStack(Items.melon));
                             EntityItem egos = new EntityItem(worldObj, (xCoord - 5) + i, yCoord - 1, (zCoord - 5) + j, new ItemStack(Nukesfromthefuture.my_ego));
                             EntityItem test = new EntityItem(worldObj, (xCoord - 5) + i, yCoord - 1, (zCoord - 5) + j, new ItemStack(Items.nether_star));
                             worldObj.spawnEntityInWorld(test);
@@ -159,11 +176,14 @@ public class TileNReactor extends TileEntity{
                             worldObj.spawnEntityInWorld(egos);
 
                         }
+
                     }
-
-
                 }
             }
+
+
+
+
         }
     }
 

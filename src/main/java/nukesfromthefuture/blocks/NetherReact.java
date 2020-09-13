0973a937216f51f9d53fb194997c4e597cbef95c
elite_mlgbro <you@example.com>
2020-09-13 14:02:01 -0600

    I finished nether react :D

--- src/main/java/nukesfromthefuture/blocks/NetherReact.java
+++ src/main/java/nukesfromthefuture/blocks/NetherReact.java
@@ -5,12 +5,16 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
+import nukesfromthefuture.Nukesfromthefuture;
 import nukesfromthefuture.tileentity.TileNReactor;
 
 public class NetherReact extends BlockContainer {
@@ -21,18 +25,26 @@ public class NetherReact extends BlockContainer {
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
         TileNReactor entity = (TileNReactor) world.getTileEntity(x, y, z);
-        if(world.isRemote) {
+
             if (!player.isSneaking()) {
                 if (!entity.isValid()) {
-                    player.addChatComponentMessage(new ChatComponentText("structure not correctly set up"));
+                    if(world.isRemote) {
+                        player.addChatComponentMessage(new ChatComponentText("Structure not correctly set up"));
+                    }
                 } else if (entity.isValid()) {
-                    player.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "Activated successfully"));
+                    if(world.isRemote) {
+                        player.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.YELLOW + "Activated successfully"));
+                    }
                     entity.buildSpire(world, x, y, z);
-                    entity.spawnEntites(world);
+                    world.setBlockToAir(x, y, z);
                     entity.replaceBlocks(world, x, y, z);
+                } else if(entity.Activated() && !entity.isValid()){
+                    if(world.isRemote){
+                        player.addChatComponentMessage(new ChatComponentText("It's already been activated bruh"));
+                    }
                 }
             }
-        }
+
         return super.onBlockActivated(world, x, y, z, player, p_149727_6_, p_149727_7_, p_149727_8_, p_149727_9_);
     }
 
--- src/main/java/nukesfromthefuture/blocks/RedObsidian.java
+++ src/main/java/nukesfromthefuture/blocks/RedObsidian.java
@@ -1,10 +1,16 @@
 package nukesfromthefuture.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 
 public class RedObsidian extends Block{
     public RedObsidian(Material iron) {
         super(iron);
+    }
+
+    @Override
+    public MapColor getMapColor(int p_149728_1_) {
+        return MapColor.redColor;
     }
 }

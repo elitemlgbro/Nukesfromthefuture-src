6e0e656510bf9db38b4a068434522886279fdbc6
elite_mlgbro <you@example.com>
2020-10-11 18:40:07 -0600

    If I make you fear taco bell, I'm not sorry

---
+++ src/main/java/nukesfromthefuture/items/NuclearTaco.java
@@ -0,0 +1,28 @@
+package nukesfromthefuture.items;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import nukesfromthefuture.NffDamageSource;
+import nukesfromthefuture.Nukesfromthefuture;
+import nukesfromthefuture.entity.Mk3Explosion;
+
+public class NuclearTaco extends ItemFood{
+    public NuclearTaco(int p_i45339_1_, float p_i45339_2_, boolean p_i45339_3_) {
+        super(p_i45339_1_, p_i45339_2_, p_i45339_3_);
+    }
+
+    @Override
+    protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player) {
+        player.attackEntityFrom(NffDamageSource.nuke_diarrhea, 1);
+        Mk3Explosion entity = new Mk3Explosion(world);
+        entity.posX = player.posX;
+        entity.posY = player.posY;
+        entity.posZ = player.posZ;
+        entity.destructionRange = Nukesfromthefuture.Boystrength;
+        entity.speed = Nukesfromthefuture.colliderSpeed;
+        world.spawnEntityInWorld(entity);
+
+    }
+}

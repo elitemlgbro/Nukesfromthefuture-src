6df837df523145cbd2562c1d9d4c6786acdd8c1f
elite_mlgbro <you@example.com>
2020-10-15 10:33:43 -0600

    got rid of useless stuff

--- src/main/java/nukesfromthefuture/creativeTabs/Machines.java
+++ src/main/java/nukesfromthefuture/creativeTabs/Machines.java
@@ -21,39 +21,4 @@ public class Machines extends CreativeTabs {
 		return new ItemStack(Item.getItemFromBlock(Nukesfromthefuture.unrefinary)).getItem();
 	}
 
-	@Override
-	public void displayAllReleventItems(List list) {
-		super.displayAllReleventItems(list);
-		List<ItemStack> bat = new ArrayList();
-		for(Object o : list){
-			if(o instanceof ItemStack){
-				ItemStack stack = (ItemStack) o;
-				if(stack.getItem() instanceof IBatteryItem){
-					bat.add(stack);
-				}
-			}
-		}
-		for(ItemStack stack : bat) {
-
-			if(!(stack.getItem() instanceof IBatteryItem)) //shouldn't happen but just to make sure
-				continue;
-
-			IBatteryItem battery = (IBatteryItem) stack.getItem();
-
-			ItemStack empty = stack.copy();
-			ItemStack full = stack.copy();
-
-			battery.setCharge(empty, 0);
-			battery.setCharge(full, battery.getMaxCharge());
-
-			int index = list.indexOf(stack);
-
-			list.remove(index);
-			list.add(index, full);
-
-			//do not list empty versions of SU batteries
-			if(battery.getChargeRate() > 0)
-				list.add(index, empty);
-		}
-	}
 }

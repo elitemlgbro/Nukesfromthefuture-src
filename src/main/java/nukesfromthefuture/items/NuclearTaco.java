f3a561c3138ffbbae5def8b8c648b233dfb139fc
elite_mlgbro <you@example.com>
2020-10-21 11:01:57 -0600

    Made sure it gives you nuke diarrhea achievement

--- src/main/java/nukesfromthefuture/items/NuclearTaco.java
+++ src/main/java/nukesfromthefuture/items/NuclearTaco.java
@@ -9,6 +9,7 @@ import nukesfromthefuture.Nukesfromthefuture;
 import nukesfromthefuture.entity.Mk3Explosion;
 
 public class NuclearTaco extends ItemFood{
+    /**Darn u Taco Bell*/
     public NuclearTaco(int p_i45339_1_, float p_i45339_2_, boolean p_i45339_3_) {
         super(p_i45339_1_, p_i45339_2_, p_i45339_3_);
     }

b41328d16b75d89e760e3a7d799be1e8fcadf8d8
elite_mlgbro <you@example.com>
2020-10-24 15:22:00 -0600

    so this is a thing??

---
+++ src/main/java/nukesfromthefuture/blocks/Begonium.java
@@ -0,0 +1,11 @@
+package nukesfromthefuture.blocks;
+
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fluids.BlockFluidClassic;
+import net.minecraftforge.fluids.Fluid;
+
+public class Begonium extends BlockFluidClassic {
+    public Begonium(Fluid fluid, Material material){
+        super(fluid, material);
+    }
+}
---
+++ src/main/java/nukesfromthefuture/blocks/Egonium.java
@@ -0,0 +1,12 @@
+package nukesfromthefuture.blocks;
+
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fluids.BlockFluidClassic;
+import net.minecraftforge.fluids.Fluid;
+
+public class Egonium extends Fluid {
+
+    public Egonium(String fluidName) {
+        super(fluidName);
+    }
+}

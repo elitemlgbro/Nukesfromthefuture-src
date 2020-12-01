0b763f7c91388d7b1ce97d9408e7d357e5e7fc8f
elite_mlgbro <you@example.com>
2020-12-01 07:46:14 -0700

    I finally got the energy to update the mod version in the Reference and mcmod.info files

--- src/main/java/nukesfromthefuture/Reference.java
+++ src/main/java/nukesfromthefuture/Reference.java
@@ -3,7 +3,7 @@ package nukesfromthefuture;
 public class Reference {
 	public static final String MOD_ID = "nff";
 	public static final String MOD_NAME = "elite_mlgbro's nukes from the future mod";
-	public static final String version = "1.7.10A1";
+	public static final String version = "1.7.10A1.4.1";
 	public static final String Client_Proxy_Class = "nukesfromthefuture.proxy.ClientProxy";
 	public static final String Server_Proxy_Class = "nukesfromthefuture.proxy.CommonProxy";
 	public static final String GUIFACT = "nukesfromthefuture.Factory";

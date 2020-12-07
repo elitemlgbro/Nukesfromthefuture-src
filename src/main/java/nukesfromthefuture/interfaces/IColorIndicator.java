d4f588dea98d8cbec1346b3e5fed3942edc98d94
elite_mlgbro <you@example.com>
2020-12-07 12:43:05 -0700

    An interface for tile entities that react to something by changing color

---
+++ src/main/java/nukesfromthefuture/interfaces/IColorIndicator.java
@@ -0,0 +1,10 @@
+package nukesfromthefuture.interfaces;
+
+public interface IColorIndicator {
+    float getRed();
+    float getBlue();
+    float getGreen();
+    void setRed(float i);
+    void setBlue(float i);
+    void setGreen(float i);
+}
